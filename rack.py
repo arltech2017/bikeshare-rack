347fb2ed49ef02d4ca73b5d12b093b7b8bc6fd81
CassiusClose <CassiusClose@gmail.com>
2018-03-09 12:30:42 -0500

    updated pool deletion

--- rack.py
+++ rack.py
@@ -7,7 +7,7 @@ key = "ITSAKEY"
 pool = [None] * 10
 
 def refresh_pool():
-    for i in range(10):
+    for i in range(len(pool)):
         pool[i] = truncate(hmac.new(key.encode(), str(counter + i).encode(), "sha256"))
 
 def truncate(hmac):
@@ -16,10 +16,23 @@ def truncate(hmac):
 
 def accept_code(code):
     if code in pool:
+        remove_code(code)
+        counter += 1
         return True
     return False
 
+def remove_code(code):
+    found = False
+    for i in range(len(pool)):
+        if code == pool[i]:
+            found = True
+        if found:
+            if i+1 >= len(pool):
+                pool[i] = None
+            else:
+                pool[i] = pool[i+1]
+
 refresh_pool()
 print(pool)
-
 print(accept_code("167"))
+print(pool)
