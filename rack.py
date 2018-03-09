41267d15768fd1d6ef76d22a0f2a6a965bf47e45
CassiusClose <CassiusClose@gmail.com>
2018-03-09 12:20:28 -0500

    Got pool working

--- rack.py
+++ rack.py
@@ -1,3 +1,25 @@
 import hmac
+import hashlib
 
+counter = 0
+key = "ITSAKEY"
 
+pool = [None] * 10
+
+def refresh_pool():
+    for i in range(10):
+        pool[i] = truncate(hmac.new(key.encode(), str(counter + i).encode(), "sha256"))
+
+def truncate(hmac):
+    digest = hmac.hexdigest()
+    return format(int(digest[:2], 16), "03d")
+
+def accept_code(code):
+    if code in pool:
+        return True
+    return False
+
+refresh_pool()
+print(pool)
+
+print(accept_code("167"))
