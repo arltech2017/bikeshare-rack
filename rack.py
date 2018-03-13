65deee9bcd3426af637779978bb8fe7ed602acb5
CassiusClose <CassiusClose@gmail.com>
2018-03-13 11:35:32 -0400

    Got pool timeout working

--- rack.py
+++ rack.py
@@ -1,30 +1,89 @@
 import hmac
 import hashlib
+import time
+
+class Key():
+    def __init__(self, key, n):
+        self.n = n 
+        self.key = key
+        self.invaltime = None
+
+    def __str__(self):
+        return self.key
+
+def print_pool(l):
+    print("[", end='')
+    for i in range(len(l)):
+        print(str(l[i]), end='')
+        if i < len(l) - 1:
+            print(', ', end='')
+    print(']')
+
+def print_time(l):
+    print("[", end='')
+    for i in range(len(l)):
+        if l[i]:
+            print(l[i].invaltime, end='')
+        else:
+            print("None", end='')
+
+        if i < len(l) - 1:
+            print(', ', end='')
+    print(']')
 
 counter = 0
-key = "ITSAKEY"
+secret = "ITSAKEY"
 
 pool = [None] * 10
+invallimit = 2 
 
 def refresh_pool():
+    global counter
     for i in range(len(pool)):
-        pool[i] = truncate(hmac.new(key.encode(), str(counter + i).encode(), "sha256"))
+        if not pool[i]:
+            key = truncate(hmac.new(secret.encode(), str(counter).encode(), "sha256"))
+            counter += 1
+            pool[i] = Key(key, counter)
 
 def truncate(hmac):
     digest = hmac.hexdigest()
     return format(int(digest[:2], 16), "03d")
 
 def accept_code(code):
-    if code in pool:
-        remove_code(code)
-        counter += 1
-        return True
+    for i in range(len(pool)):
+        if pool[i] and pool[i].key == code:
+            remove_code(code)
+            invalidate_codes(pool[i].n)
+            remove_inval_codes()
+            refresh_pool()
+            #counter += 1
+            return True
+    remove_inval_codes()
+    refresh_pool()
     return False
+
+def invalidate_codes(n):
+    for i in range(len(pool)):
+        if pool[i] and pool[i].invaltime == None and pool[i].n < n:
+            pool[i].invaltime = time.time()
+    
+def remove_inval_codes():
+    i = 0
+    while i < len(pool):
+        if pool[i] and not pool[i].invaltime == None:
+            diff = time.time() - pool[i].invaltime
+            if diff >= invallimit:
+                remove_code(pool[i].key)
+            else:
+                i += 1
+        else:
+            i += 1
+
 
 def remove_code(code):
     found = False
     for i in range(len(pool)):
-        if code == pool[i]:
+        if pool[i] and code == pool[i].key:
             found = True
         if found:
             if i+1 >= len(pool):
@@ -33,6 +92,12 @@ def remove_code(code):
                 pool[i] = pool[i+1]
 
 refresh_pool()
-print(pool)
+print_pool(pool)
+print_time(pool)
 print(accept_code("167"))
-print(pool)
+print_pool(pool)
+print_time(pool)
+time.sleep(3)
+print(accept_code("145"))
+print_pool(pool)
+print_time(pool)
