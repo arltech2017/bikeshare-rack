3fbbf3833621a0195db764831c24fe299537dd77
CassiusClose <CassiusClose@gmail.com>
2018-05-16 01:45:44 -0400

    added lots of documentation, todo

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 
+#TODO: Finish implementing the Counter class in the rest of the code
+#TODO: Implement multitasking so that multiple bikes can be unlocked at once
+
 import machine
 import time
 import hmac
+
+"""The hmac library's sha256 is broken, so use our version, downloaded from the micropython repo"""
 import _sha256 as sha256
 
-
+"""
+An interface between the code and the matrix membrane keypad attached to the arduino. Able to read input from the keypad.
+"""
 class Keypad():
+    """
+    The order of the keys as they show up on the keypad, used for determining the key value of a specific row/column location.
+    """
     buttons = (('1', '2', '3'),
                ('4', '5', '6'),
                ('7', '8', '9'),
                ('*', '0', '#'))
 
+    """
+    Accepts the esp pins for columns and rows, increasing along with the buttons tuples. Set column pins to output and row pins to input.
+    """
     def __init__(self, col_pins, row_pins):
         self.cols = [machine.Pin(i, machine.Pin.OUT) for i in col_pins]
         self.rows = [machine.Pin(i, machine.Pin.IN) for i in row_pins]
 
+    """
+    Returns true if any of the keys on the keypad is pressed.
+    For each column pin, output a signal. If a row pin is receiving a signal, the key at the row/column intersection is being pressed, and the corresponding string in 'buttons' is returned. If no key is pressed, return None. 
+    """
     @property
     def pressed(self):
         for i, col in enumerate(self.cols):
@@ -28,17 +45,29 @@ class Keypad():
             col.value(0)
         return None
 
+    """
+    Returns the most recently pressed key on the keypad. The first while loop waits until a key is being pressed, and then the second while loop waits until the key is released to return.
+    """
     def get_next_pressed(self):
+        #Make sure val is the most recently pressed key that is not None
         val = self.pressed
         while not val:
             val = self.pressed
-        val2 = val
-        while val:
-            val = self.pressed
-            if val:
-                val2 = val
-        return val2
 
+        val2 = val
+
+        #Sets val2 to the most recently pressed key until no key is being pressed (val == None)
+        while val:
+            val = self.pressed #get current pressed key
+
+            if val:
+                val2 = val #sets val2 to the most recent key pressed
+
+        return val2 #return most recently pressed key
+
+    """
+    Stores a list of the keys pressed as a string. Keeps recording key presses until it finds the escape character, '#', and then return the message. 
+    """
     def get_input_message(self):
         message = ""
         button = self.get_next_pressed()
@@ -47,11 +76,18 @@ class Keypad():
             button = self.get_next_pressed()
         return message
 
-
+"""
+Represents a hashed code able to unlock a bike. 12 subcodes are stored, each one linked to a bike number, any of which can be used to unlock a specific bike. 
+"""
 class Key():
+    """
+    Initializes 'keys' a list of 12 codes, one for each bike. With 'key' being the initial hash code, each code is 'key' + hash('key' + 'bike number'). 
+    'invaltime' is the timestamp that the key became invalidated (when the key is skipped, see invalidate_codes() in Pool), or None if the key is valid.
+    'n' is the value of the counter that this key was generated with. This is used to compare keys, to see which one came first. This is useful when invaliding and removing unused keys.
+    """
     def __init__(self, key, n):
         self.n = n
-        self.keys = [None] * 2
+        self.keys = [None] * 12
         for i in range(len(self.keys)):
             self.keys[i] = key + str(hotp.at(key + str(i)))
         self.invaltime = None
@@ -74,14 +110,23 @@ class Key():
         s += ']'
         return s
 
-
+"""
+An HOTP encryption class, follows the standard guidelines. Use at() to get an encrypted message.
+"""
 class HOTP():
+    #The number of digits the returned code will be
     codelen = 3
 
+    """
+    Pass this a secret and a hash function. The function should be sha256
+    """
     def __init__(self, secret, hash_func=sha256.sha256):
         self.secret = secret
         self.hash_func = hash_func
 
+    """
+    Pass a message ('counter') and it returns the HOTP encrypted form, with the hex form truncated as according to truncate()
+    """
     def at(self, counter):
         return self.truncate(
             hmac.new(
@@ -90,51 +135,56 @@ class HOTP():
                 self.hash_func
             ).hexdigest())
 
+    """
+    Takes a hexdigest from at(), turns it into an integer, and returns the first number of digits specified by 'self.codelen'
+    """
     def truncate(self, hashstr):
         return format(int(hashstr, 16) % (10 ** self.codelen),
                       '0{}d'.format(self.codelen))
 
 
+"""
+Helper method to replace String's format() because it's not supported in micropython. Accepts an object 'i' and formats according to arguments 'args' as normal format() would do.
+"""
 def format(i, args):
     argstr = '{:' + args + '}'
     return argstr.format(i)
 
-
+"""
+A pool of key objects.
+Used as backup in case someone requests a code but never uses it. Should someone request a code, it would be used up on the server side. If the code is never entered into the bike rack, it would be forever looking for that code, even when other codes are being distributed by the server. A pool of available codes is the solution.
+"""
 class Pool():
-    def __init__(self, size, encryption, inval_time_limit):
-        """Accepts three arguments:
-            -the size of the pool
-            -the class used to encrypt the counter, should be HOTP()
-            -the length of time after which an invalid code will be removed
+    """Accepts three arguments:
+        -the size of the pool
+        -the class used to encrypt the counter, should be HOTP()
+        -the length of time after which an invalid code will be removed
 
-        Initially fills up the pool
-        """
+    Initially fills up the pool
+    """
+    def __init__(self, size, encryption, counter, inval_time_limit):
         self.pool = [None] * size
         self.encryption = encryption
+        self.counter = counter
         self.inval_time_limit = inval_time_limit
         self.repopulate(0)
 
-    def repopulate(self, counter):
-        """
-        Iterates through the pool and for every None value, replaces it with a new
-        key and increments the counter
-        Since counter is being passed to it, returns the updated counter. The code
-        calling this function should set its counter to the return value of this
-        function
-        """
+    """
+    For each None value in the pool, generate a new key according to the value of 'counter' and increment the counter. Return the counter so that the counter object can be updated.
+    """
+    def repopulate(self):
         for i in range(len(self.pool)):
             if not self.pool[i]:
-                key = self.encryption.at(counter)
-                self.pool[i] = Key(key, counter)
-                counter += 1
+                key = self.encryption.at(self.counter)
+                self.pool[i] = Key(key, self.counter)
+                self.counter += 1
         print("Done repopulating pool")
-        return counter
+        return self.counter
 
+    """
+    Removes the given key from the pool if discovered, and shifts all items after it to the left. Successfully removing a key will leave a None value at the end of the list, so repopulate() will be needed.
+    """
     def remove_key(self, key):
-        """
-        Removes a key from the pool and shifts the other keys to the left to fill
-        its gap and maintain an ordered list
-        """
         found = False
         for i in range(len(self.pool)):
             if self.pool[i] and self.pool[i].keys is key.keys:
@@ -145,28 +195,22 @@ class Pool():
                 else:
                     self.pool[i] = self.pool[i + 1]
 
+    """
+    Invalidates any key whose n value is less than 'n', that is, any key that was created before the counter reached 'n'. This should be called whenever a key is accepted (because any keys before the accepted key will never be used).
+    Invalidates the keys by setting their 'invaltime' to the current time.
+    """
     def invalidate_codes(self, n):
-        """
-        Accepts a key number, presumably of a code that was just used, and marks
-        any key with a lower number as invalid by setting its invalidated time
-        """
         for i in range(len(self.pool)):
             key = self.pool[i]
             if key and key.invaltime is None and key.n < n:
                 key.invaltime = time.time()
 
-
+    """
+    Checks to see if 'code' is in the pool.
+    Iterates through the subkeys in each key and returns the subkey index (the bike number) if found.
+    Currently when a key is found, this function removes it from the pool, removes invalidated codes, invalidates any codes before the accepted key, and then reopulates the pool, all before returning the bike index. This means there's a significant time gap as the microcontroller generates a new key, and should be fixed.
+    """
     def accept_code(self, code):
-        """
-        Takes a string and returns whether the string is in the pool as a key. If
-        the code is found, remove it from the pool and mark any previous codes as
-        invalid.
-        Whether or not the code is found, remove any code that has been invalid for
-        too long and then refresh the pool with new codes.
-        """
-
-        global counter #Not needed here, but a reminder that counter will need to be updated after this method
-
         for i in range(len(self.pool)):
             key = self.pool[i]
             if key:
@@ -175,19 +219,15 @@ class Pool():
                         self.remove_key(key)
                         self.remove_inval_codes()
                         self.invalidate_codes(key.n)
-                        counter = self.repopulate(counter) #update counter here
+                        self.counter = self.repopulate(self.counter) #update counter here
                         return j
 
         return None
 
+    """
+    Removes any key that has been invalidated for greater than the time limit, as designated by 'self.inval_time_limit'
+    """
     def remove_inval_codes(self):
-        """
-        Removes any code that has been invalid for too long (that is, longer than
-        global invallimit)
-        Uses while() instead of for() because once a code is removed, everything shifts over an
-        index, so you want to check that index again.
-        """
-
         i = 0
         while i < len(pool):
             found = False
@@ -215,7 +255,8 @@ class Pool():
         s += "]"
         return s
 
-
+"""A class for storing the 'counter' int, which is incremented and used to generate the passcodes. Reads and writes to a file on the microcontroller to survive reboot.
+"""
 class Counter():
     def __init__(self, filename, start=None):
         self.filename = filename
@@ -236,30 +277,47 @@ class Counter():
         self._set(num)
         return num
 
+"""
+An interface for the relay, which physically unlocks the bikes on the rack.
+As of currently, no new code can be accepted while a bike is unlocked, meaning only one bike can be unlocked at the same time. This needs to change.
+"""
 class Relay():
+    unlocktime = 5 #How many seconds the bikes are unlocked for
+
+    """
+    Sets up the pins connected to the relay as output, as passed by 'pins'.
+    HIGH is closed, so output a signal for all pins.
+    """
     def __init__(self, pins):
         self.pins = [machine.Pin(i, machine.Pin.OUT) for i in pins]
         for pin in self.pins:
             pin.value(1)
 
+    """
+    Unlocks the bike of 'bike_num', which corresponds to 'self.pins' index
+    """
     def unlock_bike(self, bike_num):
         self.pins[bike_num].value(0)
-        time.sleep(5)
+        time.sleep(self.unlocktime)
         self.pins[bike_num].value(1)
 
+#Turn ESP32 blue light on for dev testing
 pin2 = machine.Pin(2, machine.Pin.OUT)
 pin2.value(1)
+
 kp = Keypad((21, 22, 23), (16, 17, 18, 19))
 hotp = HOTP("ITSAKEY", sha256.sha256)
-pool = Pool(10, hotp, 3600) #set invalid time to an hour (3600 seconds)
 counter = 10 #Set counter to 10 initially because calling Pool() initializes the first 10 keys
-relay = Relay((4, 15, 10, 9, 13, 12, 14, 27, 26, 25, 33, 32))
+pool = Pool(10, hotp, counter 3600) #set invalid time limit to an hour (3600 seconds)
+relay = Relay((4, 0, 15, 10, 9, 13, 14, 27, 26, 25, 33, 32))
 
+#Turn ESP32 blue light off to signify setup completion
 pin2.value(0)
-print(pool)
+#print(pool)
 
+"""Main loop, continually tries to accept input from the keypad, and unlocks the bike if the input is accepted as a code."""
 while True:
     result = pool.accept_code(kp.get_input_message())
-    print(result)
+    #print(result)
     if result is not None:
         relay.unlock_bike(result)
